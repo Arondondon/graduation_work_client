36d98fc69facbe0f0f9ed3c71e13b03f06fdb65b
Dronny <andreypravdin52@gmail.com>
2024-05-15 03:05:25 +0300

    Now errors of the first and second types are calculated, both in general and separately for each type of testing. You can also now view all image pairs from failed tests.

--- src/actions/testing.py
+++ src/actions/testing.py
@@ -1,5 +1,8 @@
 # D:/PROGRAMMING/py_face_rec/venv/Scripts/python.exe D:/PROGRAMMING/py_face_rec/compare.py
 
+# ошибка первого рода - false accept (неверно принято)
+# ошибка второго рода - false reject (неверно отвергнуто)
+
 from src.actions.server_requests import ServerConnection
 from src.ui.ui_results import Ui_resultsDialog
 from src.ui.ui_testing_progress import Ui_testingProgressDialog
@@ -21,6 +24,19 @@ class Testing:
         if not os.path.exists(self.images_dir):
             os.mkdir(self.images_dir)
         self.images_dir += "/"
+
+        self.failed_test_images_example = {
+            "first_image": "",
+            "second_image": "",
+            "expected_result": False,
+            "actual_result": False
+        }
+
+        self.section_results_count_example = {
+            "failed_tests_total": 0,
+            "type_one_errors": 0,
+            "type_two_errors": 0
+        }
 
         self.TESTING_OPTIONS = ["Simple Testing",
                                 "Testing with tag GRIMACE",
@@ -64,21 +80,28 @@ class Testing:
         self.tests_passed_in_total = 0
         self.last_progress = 0
         self.cmd = ""
-        self.failed_tests = []
-        self.failed_tests_in_sections_count = []
         self.current_testing_options = []
+
+        self.failed_tests = []
+        self.index_of_failed_test = -1
+        self.failed_tests_in_sections_count = []
         self.texts_for_results = []
 
     def get_testing_options(self) -> [str]:
         return self.TESTING_OPTIONS
 
     def start(self, cmd: str, testing_options: [str]):
+        if len(testing_options) == 0:
+            return
         self.current_testing_options = testing_options
         self.failed_tests.clear()
         self.cmd = cmd
         self.sections_in_total = len(testing_options)
         self.failed_tests_in_sections_count.clear()
-        self.failed_tests_in_sections_count = [0] * self.sections_in_total
+        self.failed_tests_in_sections_count = [
+            self.section_results_count_example.copy() for _ in range(self.sections_in_total)
+        ]
+        self.index_of_failed_test = -1
         self.tests_passed_in_total = 0
         self.update_progress(self.sections_in_total, 1, 0, 0, 0)
         self.texts_for_results.clear()
@@ -130,9 +153,29 @@ class Testing:
                 expected_result = images[i]["person"] == images[j]["person"]
                 actual_result = self.compare_two_images(images[i]["uri"], images[j]["uri"])
 
+                #         test failed images
+                # d = self.failed_test_images_example.copy()
+                # d["first_image"] = images[i]["uri"]
+                # d["second_image"] = images[j]["uri"]
+                # d["expected_result"] = expected_result
+                # d["actual_result"] = actual_result
+                # self.failed_tests.append(d)
+                #         end of test
+
                 if expected_result != actual_result:
-                    self.failed_tests.append([i, j])
-                    self.failed_tests_in_sections_count[self.current_section_number - 1] += 1
+                    d = self.failed_test_images_example.copy()
+                    d["first_image"] = images[i]["uri"]
+                    d["second_image"] = images[j]["uri"]
+                    if expected_result:
+                        d["expected_result"] = True
+                        d["actual_result"] = False
+                        self.failed_tests_in_sections_count[self.current_section_number - 1]["type_two_errors"] += 1
+                    else:
+                        d["expected_result"] = False
+                        d["actual_result"] = True
+                        self.failed_tests_in_sections_count[self.current_section_number - 1]["type_one_errors"] += 1
+                    self.failed_tests.append(d)
+                    self.failed_tests_in_sections_count[self.current_section_number - 1]["failed_tests_total"] += 1
 
                 tests_left_in_section -= 1
                 self.tests_passed_in_total += 1
@@ -146,7 +189,15 @@ class Testing:
     def set_results(self):
         self.ui_results.firstStatValue.setText(str(self.sections_in_total))
         self.ui_results.secondStatValue.setText(str(self.tests_passed_in_total))
-        self.ui_results.thirdStatValue.setText(str(sum(self.failed_tests_in_sections_count)))
+
+        failed_tests_total_count = sum([d["failed_tests_total"] for d in self.failed_tests_in_sections_count])
+        self.ui_results.thirdStatValue.setText(str(failed_tests_total_count))
+
+        type_one_errors_count = sum([d["type_one_errors"] for d in self.failed_tests_in_sections_count])
+        self.ui_results.fourthStatValue.setText(str(type_one_errors_count))
+
+        type_two_errors_count = sum([d["type_two_errors"] for d in self.failed_tests_in_sections_count])
+        self.ui_results.fifthStatValue.setText(str(type_two_errors_count))
 
         self.ui_results.sectionsComboBox.clear()
         self.ui_results.sectionsComboBox.addItems(self.current_testing_options)
@@ -154,17 +205,59 @@ class Testing:
         self.ui_results.sectionsComboBox.currentIndexChanged.connect(self.result_item_changed)
 
         self.ui_results.resultsTextEdit.setText(self.texts_for_results[0])
-        self.ui_results.firstImage.setPixmap(QPixmap(self.images_dir + "test_image_1.jpg"))
-        self.ui_results.secondImage.setPixmap(QPixmap(self.images_dir + "test_image_2.jpg"))
+
+        self.ui_results.nextButton.clicked.connect(self.next_images_button_clicked)
+        self.ui_results.previousButton.clicked.connect(self.previous_images_button_clicked)
+        if len(self.failed_tests) > 0:
+            self.index_of_failed_test = 0
+            self.set_failed_images(self.failed_tests[0])
 
     def add_result_text(self, option: str, tests_in_total: int):
+        failed_tests_in_section_count = self.failed_tests_in_sections_count[self.current_section_number - 1]
+
         text = self.INITIAL_TEXTS_FOR_RESULTS[option]
         text += option + " results:\n"
         text += "Total number of tests: " + str(tests_in_total) + "\n"
-        text += "Tests passed: " + str(
-            tests_in_total - self.failed_tests_in_sections_count[self.current_section_number - 1]) + "\n"
-        text += "Tests failed: " + str(self.failed_tests_in_sections_count[self.current_section_number - 1]) + "\n"
+        text += "Tests passed: " + str(tests_in_total - failed_tests_in_section_count["failed_tests_total"]) + "\n"
+        text += "Tests failed: " + str(failed_tests_in_section_count["failed_tests_total"]) + "\n"
+        text += "Type I (false accept) errors: " + str(failed_tests_in_section_count["type_one_errors"]) + "\n"
+        text += "Type II (false reject) errors: " + str(failed_tests_in_section_count["type_two_errors"]) + "\n"
+
         self.texts_for_results.append(text)
+
+    def next_images_button_clicked(self):
+        if self.index_of_failed_test == -1:
+            return
+        self.index_of_failed_test += 1
+        self.index_of_failed_test %= len(self.failed_tests)
+        self.set_failed_images(self.failed_tests[self.index_of_failed_test])
+
+    def previous_images_button_clicked(self):
+        if self.index_of_failed_test == -1:
+            return
+        self.index_of_failed_test -= 1
+        self.index_of_failed_test %= len(self.failed_tests)
+        self.set_failed_images(self.failed_tests[self.index_of_failed_test])
+
+    def set_failed_images(self, failed_test: dict):
+        file_path_1 = self.images_dir + "result_image_1.jpg"
+        file_path_2 = self.images_dir + "result_image_2.jpg"
+
+        image_bytes_1 = self.conn.download_image(failed_test["first_image"])
+        image_bytes_2 = self.conn.download_image(failed_test["second_image"])
+
+        with open(file_path_1, "wb") as f:
+            f.write(image_bytes_1)
+        with open(file_path_2, "wb") as f:
+            f.write(image_bytes_2)
+
+        self.ui_results.firstImage.setPixmap(QPixmap(file_path_1))
+        self.ui_results.secondImage.setPixmap(QPixmap(file_path_2))
+        self.ui_results.expectedStatValue.setText(str(failed_test["expected_result"]))
+        self.ui_results.actualStatValue.setText(str(failed_test["actual_result"]))
+
+        os.remove(file_path_1)
+        os.remove(file_path_2)
 
     def result_item_changed(self, index: int):
         self.ui_results.resultsTextEdit.setText(self.texts_for_results[index])
